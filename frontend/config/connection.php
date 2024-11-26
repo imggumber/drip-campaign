e010e2f10ac07ead6beaacc3e1b42b26c8d8caf9
Gurpreet Gumber <gumber.gurpreet@rwit.io>
2024-11-26 17:35:32 +0530

    Call connection function

--- frontend/config/connection.php
+++ frontend/config/connection.php
@@ -3,13 +3,7 @@
 $logFolder = 'logs';
 $errorLogFile = $logFolder . '/error.log';
 
-try {
-  $conn = new PDO("mysql:host=".HOSTNAME.";dbname=".DATABASE."", USERNAME, PASSWORD);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    add_log($e->getMessage());
-    echo "<h1>Failed to establish connection.</h1>";
-    die;
-}
-?>
+establish_connection();
+
+
+
