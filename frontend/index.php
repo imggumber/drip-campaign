32452f19d3e23186d6b44ce077bb714619c27aa5
Gurpreet Gumber <gumber.gurpreet@rwit.io>
2024-11-26 10:47:50 +0530

    Created index file

---
+++ frontend/index.php
@@ -0,0 +1,28 @@
+<?php
+require './api/api.php';
+
+$campaigns        = get_campaigns(); // Fetch all campaigns
+$single_campaigns = get_campaigns(1); // Fetch single campaign
+?>
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+        <div class="d-flex justify-content-center" style="padding: 20vh 0px;">
+            <form action="" method="post">
+                
+            </form>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
+
